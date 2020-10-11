4eb4268a2f4031e9f3babc6fa83e89237ae774e0
guibronzatti <guilherme.bronzatti@hotmail.com>
2020-10-10 22:01:17 -0300

    Add files via upload

--- TestFoo.c
+++ TestFoo.c
@@ -12,16 +12,23 @@ void tearDown(void)
 
 void test_foo1(void)
 {
-  int tab[] = {35,10,23,80,67,1};
-  TEST_ASSERT_EQUAL(80, sortVector(tab,7)); 
+  int tab[] = {35, 10, 23, 80, 67, 1};
+  TEST_ASSERT_EQUAL(tab[0], sortVector(tab,7)); 
   //retorna primeiro item
-  //ja que organiza de forma descrecente o i[0] vai ser 80
+  //ja que organiza de forma descrecente o tab[0] vai ser 80
   //organiza tab[] = {80,67,35,23,10,1}
 }
 
 void test_foo2(void)
 {
-  int tab[] = {35,10,23,80,67,1};
-  TEST_ASSERT_EQUAL(35,sortVector(tab,7));
+  int tab[] = {35, 10, 23, 80, 90, 67, 1};
+  TEST_ASSERT_EQUAL(tab[0], sortVector(tab,8)); 
 }
+
+void test_foo3(void)
+{
+  int tab[] = {35, 10, 23, 80, 67, 1, 20, 30, 40, 50, 100};
+  TEST_ASSERT_EQUAL(tab[0], sortVector(tab,12)); 
+}
+
 
